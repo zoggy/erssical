197410f3189218179eab323da137c7e581211496
Zoggy <zoggy@bat8.org>
2013-03-26 08:26:48 +0100

    handle errors in curl

--- ers_curl.ml
+++ ers_curl.ml
@@ -7,12 +7,20 @@ let writer b data =
   String.length data
 ;;
 
+let handle_curl_error f x =
+  try f x
+  with Curl.CurlException (curl_code, n, reason) ->
+    failwith ("Curl: " ^ reason)
+
 let get url =
   let buf = Buffer.create 256 in
-  let connection = Curl.init () in
-  Curl.set_url connection (Ers_types.string_of_url url);
-  Curl.set_writefunction connection (writer buf);
-  Curl.perform connection;
-  Curl.cleanup connection;
-  Buffer.contents buf
+  let connection = handle_curl_error Curl.init () in
+  let f () =
+    Curl.set_url connection (Ers_types.string_of_url url);
+    Curl.set_writefunction connection (writer buf);
+    Curl.perform connection;
+    Buffer.contents buf
+  in
+  try handle_curl_error f ()
+  with e -> Curl.cleanup connection; raise e
 ;;
